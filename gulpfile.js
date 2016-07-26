af11091aab731677fc4bec1aa11973570cf79c8a
talesbaz <talesbaz@gmail.com>
2016-07-26 00:46:56 -0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp   = require('gulp'),
     eslint = require('gulp-eslint');
 
 gulp.task('test', function () {
-  return gulp.src([ '*.js', 'actions/*', '!node_modules/*', '!gulpfile.js' ])
+
+  return gulp.src([ '*.js', 'actions/*', '!node_modules/', '!gulpfile.js' ])
               .pipe(eslint())
               .pipe(eslint.format())
               .pipe(eslint.failAfterError());
