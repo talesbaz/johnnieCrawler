1eece06540833cbf1cd73e7dc9f696b1c867d2c8
talesbaz <talesbaz@gmail.com>
2016-07-26 00:33:07 -0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,18 @@
 /**
  * Package Dependencies
  */
-var gulp        = require('gulp'),
-    eslint      = require('gulp-eslint');
+var gulp   = require('gulp'),
+    eslint = require('gulp-eslint');
 
 gulp.task('eslint', () => {
 
-  return gulp.src([ '*.js', 'actions/*','!node_modules/*', '!gulpfile.js' ])
+    return gulp.src([ '*.js', 'actions/*', '!node_modules/*', '!gulpfile.js' ])
                 .pipe(eslint())
                 .pipe(eslint.format())
                 .pipe(eslint.failAfterError());
 
 });
 
-gulp.task('test', ['eslint'], function (){});
+gulp.task('test', ['eslint'], function () {
+
+});
